661d6bc3dced037627fb106f02709adcf4a14971
claireisabelle <claire.bourdale@gmail.com>
2018-06-04 20:38:33 +0200

    Initialisation projet

---
+++ front-page.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Document</title>
+<?php wp_head();  ?>
+</head>
+
+<body>
+
+<header>
+	<div class="header-top">
+		<div class="container">
+			<nav class="header-top-menu">
+				<ul>
+					<li><a href="">English</a></li>
+					<li>|</li>
+					<li><a href="">Qui sommes-nous ?</a></li>
+					<li><a href="">Avis</a></li>
+					<li><a href="">Contact</a></li>
+
+				</ul>
+			</nav>
+		</div>
+		<!-- /.container -->
+	</div>
+	<!-- /.header-top -->
+
+	<div class="header-logo">
+		H'OMAHA <span class="logo-part2">NATURE</span>
+	</div>
+	<!-- /.header-logo -->
+
+	<div class="header-menu">
+		<nav>
+			<ul>
+				<li><a href="">Gîte</a></li>
+				<li><a href="">Activités</a></li>
+				<li><a href="">A proximité</a></li>
+				<li><a href="">Tarifs</a></li>
+				<li><a href="">Réservations</a></li>
+			</ul>
+		</nav>
+	</div>
+	<!-- /.header-menu -->
+
+
+</header>
+	
+</body>
+</html>
