e129dc2b184b4f0cec258f67851f635fdfaaafa9
claireisabelle <claire.bourdale@gmail.com>
2018-06-10 12:28:54 +0200

    Initialisation bandeau diaporama

--- front-page.php
+++ front-page.php
@@ -45,8 +45,40 @@
 	</div>
 	<!-- /.header-menu -->
 
-
 </header>
+
+<div class="bandeau">
+	<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img class="d-block w-100" src="<?php bloginfo('template_url'); ?>/img/bandeau.jpg" alt="First slide">
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="<?php bloginfo('template_url'); ?>/img/bandeau.jpg" alt="Second slide">
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="<?php bloginfo('template_url'); ?>/img/bandeau.jpg" alt="Third slide">
+			</div>
+		</div>
+
+		<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div>
+	<div class="triangle"></div>
+	<!-- /.triangle -->
+</div>
+<!-- /.bandeau -->
+
+
+
+
 	
+<?php wp_footer(); ?>	
 </body>
 </html>
